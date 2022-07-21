8c59b900c44e1650d09fd9fe20890b4791f8f200
Tani Kaye <1tanikaye@gmail.com>
2022-07-20 21:44:28 -0400

    basically finished

---
+++ twitch/pages/index.js
@@ -0,0 +1,10 @@
+import { getSession } from "next-auth/react"
+import Layout from '../components/Layout'
+
+export default function Home() {
+  return (
+    <div>
+      <Layout />
+    </div>
+  )
+}
